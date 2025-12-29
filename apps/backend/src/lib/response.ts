02d717b8b102ec14c2e9ac2cbe6627878b5d717e
Javier Batres <fjbatresvgmail.com>
2025-12-28 20:11:57 -0600

    expand cors response headers

--- apps/backend/src/lib/response.ts
+++ apps/backend/src/lib/response.ts
@@ -4,6 +4,8 @@ export function jsonResponse<T>(statusCode: number, body: T) {
     headers: {
       'content-type': 'application/json',
       'access-control-allow-origin': '*',
+      'access-control-allow-headers': 'authorization,content-type',
+      'access-control-allow-methods': 'GET,POST,OPTIONS',
     },
     body: JSON.stringify(body),
   };
