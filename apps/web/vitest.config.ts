3482dd37802f7f08bfa199e7ce22a8406144881a
Javier Batres <fjbatresv@gmail.com>
2026-01-01 21:34:24 -0600

    Updated the coverage to generate lcov files for sonar

--- apps/web/vitest.config.ts
+++ apps/web/vitest.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
     globals: true,
     coverage: {
       provider: 'v8',
-      reportsDirectory: '../../coverage/apps/web',
+      reportsDirectory: 'coverage/apps/web',
       reporter: ['lcov', 'text', 'text-summary'],
       exclude: [
         '**/*.spec.ts',
